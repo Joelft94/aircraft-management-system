b424a647e0dd2c3eb28fad6cd816e662b1ce7c51
Joel Ferreira <joel_ferrer_94@hotmail.com>
2024-09-15 02:08:21 -0400

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Add your configurations here
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      use: 'js-yaml-loader',
+    })
+    return config
+  },
+}
 
 export default nextConfig;
